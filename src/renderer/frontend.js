78e68152e7b5c9c69cc761a38c08d797288a9113
Karl Heitmann <khf1988@gmail.com>
2019-11-05 22:31:19 -0300

    colocado codigo para correr python, pero no funciona

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -20,19 +20,33 @@ window.addEventListener('load', () => {
   */
 })
 function addClickEvent() {
-  debugger
   console.log("Event clicked")
   const btnOpenDir = document.getElementById('openDirectory')
   console.log("Event clicked")
   //console.log(tmp)
   console.log(btnOpenDir)
   console.log("ese fue el dom")
-  btnOpenDir.addEventListener('click', function () {
-    console.log("clicked!!!")
-  })
+  btnOpenDir.addEventListener('click', botonClick)
 }
 function botonClick() {
   console.log("Clicked!")
+  var spawn = require("child_process").spawn
+  console.log("1")
+  var process = spawn('python', ["./hola_mundo.py",
+    ""
+  ]);
+  console.log("2")
+  /*
+  var process = spawn('python', ["./hola_mundo.py",
+    req.query.funds, // starting funds
+    req.query.size, // (initial) wager size
+    req.query.count, // wager count — number of wagers per sim
+    req.query.sims // number of simulations
+  ]);
+  */
+  process.stdout.on('data', function (data) {
+    res.send(data.toString());
+  });
 }
 
 /*
