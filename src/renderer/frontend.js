bca2999ab4ef50deeffc731a596b9909fabea463
Karl Heitmann <khf1988@gmail.com>
2019-11-05 22:14:23 -0300

    agregado el frontend.js

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -9,7 +9,8 @@ import { setIpc, sendIpc } from './ipcRendererEvents'
 
 window.addEventListener('load', () => {
   console.log('VA a mandar IPC')
-  botonClick()
+  addClickEvent()
+  //botonClick()
   /*
   setIpc()
   addImagesEvents()
@@ -19,76 +20,26 @@ window.addEventListener('load', () => {
   */
 })
 function addClickEvent() {
+  debugger
   console.log("Event clicked")
+  const btnOpenDir = document.getElementById('openDirectory')
+  console.log("Event clicked")
+  //console.log(tmp)
+  console.log(btnOpenDir)
+  console.log("ese fue el dom")
+  btnOpenDir.addEventListener('click', function () {
+    console.log("clicked!!!")
+  })
 }
 function botonClick() {
   console.log("Clicked!")
 }
 
+/*
 function openDirectory() {
   const openDirectory = document.getElementById('openDirectory')
   openDirectory.addEventListener('click', () => {
     sendIpc()
   })
 }
-
-function addImagesEvents () {
-  const thumbs = document.querySelectorAll('li.list-group-item')
-
-  for (let i = 0, length1 = thumbs.length; i < length1; i++) {
-    thumbs[i].addEventListener('click', function () {
-      changeImage(this)
-    })
-  }
-}
-
-function selectEvent () {
-  const select = document.getElementById('filters')
-  const tmp = document.getElementById('image-displayed')
-  console.log(tmp)
-  select.addEventListener('change', function () {
-    applyFilter(this.value, tmp)
-  })
-}
-
-function changeImage (node) {
-  if (node) {
-    document.querySelector('li.selected').classList.remove('selected')
-    node.classList.add('selected')
-    document.getElementById('image-displayed').src = node.querySelector('img').src
-  } else {
-    document.getElementById('image-displayed').src = ''
-  }
-}
-
-function searchImagesEvent() {
-  const searchBox = document.getElementById('search-box')
-
-  searchBox.addEventListener('keyup', function () {
-    const regex = new RegExp(this.value.toLowerCase(), 'gi')
-    const val = this.value;
-    const thumbs = document.querySelectorAll('li.list-group-item img')
-    if (val.length > 0 ) {
-      for (let i = 0, length1 = thumbs.length; i < length1; i++) {
-        const fileUrl = url.parse(thumbs[i].src)
-        const fileName = path.basename(fileUrl.pathname)
-        if (fileName.match(regex)) {
-          thumbs[i].parentNode.classList.remove('hidden')
-        } else {
-          thumbs[i].parentNode.classList.add('hidden')
-        }
-      }
-    } else {
-      const hidden = document.querySelectorAll('li.hidden')
-      for (let i = 0, length1 = hidden.length; i < length1; i++) {
-        hidden[i].classList.remove('hidden')
-      }
-    }
-    selectFirstImage()
-  })
-}
-
-function selectFirstImage() {
-  const image = document.querySelector('li.list-group-item:not(.hidden)')
-  changeImage(image)
-}
+*/
